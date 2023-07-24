a674e15484d1e65653d11ae50a60838c37186bf9
Cuckss <122050270+Cuckss@users.noreply.github.com>
2023-07-24 12:58:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+let selectElement=document.getElementById("colorSelect");
+let removeButton=document.getElementsByTagName("input")[0];
+removeButton.addEventListener("click",()=>{
+	var selectedOptionIndex=selectElement.selectedIndex;
+	if(selectedOptionIndex!==-1){
+		selectElement.removeChild(selectElement.options[selectedOptionIndex])
+	}
+})
